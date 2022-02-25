d2b2e0232dd4643fc17ce6ae02e2df799ed99614
rudolf0328 <ck0716080@gmail.com>
2022-02-25 19:57:44 +0900

    feat: my circles page implement

--- modu/Pages/HomePage/myCircle.js
+++ modu/Pages/HomePage/myCircle.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, FlatList } from 'react-native';
 import colors from '../../constants/colors';
+// import SearchBar from './serachBar';
+import { SearchBar } from 'react-native-elements';
+// import CardView from './card';
+import Circle from './circle';
 
-const MyCircle = () => {
+const AllCircle = (props) => {
+  const { circles } = props;
+  const [search, setSearch] = useState("");
+  const [containerWidth, setContainerWidth] = useState(0);
+
+  const margins = 39 * 2;
+  const numColumns = 2;
+
+  const updateSearch = (search) => {
+    setSearch(search);
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <Text>MyCircle</Text>
+      <View style={styles.circles}>
+        <FlatList
+          keyExtractor={item => item.toString()}
+          scrollEnabled={true}
+          data={circles}
+          numColumns={2}
+          columnWrapperStyle={{
+            justifyContent: "space-evenly",
+            marginBottom: 23,
+            // marginVertical: 15
+            // marginHorizontal: 40
+          }}
+          // onLayout={e => setContainerWidth(e.nativeEvent.layout.width)}
+          renderItem={({item}) => <Circle circleName={item} />}
+          />
+        {/* <Text>All Circle</Text> */}
+      </View>
     </SafeAreaView>
   );
 };
@@ -16,7 +46,19 @@ const styles = StyleSheet.create({
     backgroundColor: colors.background,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  searchBar: {
+    // flex: 1,
+    width: '100%',
+    height: 50,
+    // backgroundColor: '#fff'
+  },
+  circles: {
+    flex: 1,
+    width: '100%',
+    marginTop: 25
+    // backgroundColor: '#fff'
   }
 });
 
-export default MyCircle;
+export default AllCircle;
